8cbffa0abd69cf7332d596c5ba5e2eb19826bc15
matthesrieke <m.rieke@52north.org>
2018-04-19 22:15:14 +0200

    alternative impl of point distance

--- src/de/hsbochum/fbg/kswe/aufgabe2/Point.java
+++ src/de/hsbochum/fbg/kswe/aufgabe2/Point.java
@@ -15,7 +15,17 @@ public class Point {
     }
 
     public double distanceTo(Point other) {
-        return 0.0;
+        double radius = 6371000;
+
+        double latDelta = Math.toRadians(Math.abs(this.latitude - other.latitude));
+        double lonDelta = Math.toRadians(Math.abs(this.longitude - other.longitude));
+        double a = Math.sin(latDelta/2) * Math.sin(latDelta/2) +
+                Math.cos(Math.toRadians(this.latitude)) * Math.cos(Math.toRadians(other.latitude)) *
+                Math.sin(lonDelta/2) * Math.sin(lonDelta/2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return (double) (radius * c);
     }
 
 }
